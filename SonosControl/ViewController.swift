015498dae1866f5f8d75b9a84a585cf6687a1b46
Max von Webel <max@343max.de>
2018-10-16 11:39:29 -0700

    Initial Commit

---
+++ SonosControl/ViewController.swift
@@ -0,0 +1,14 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
